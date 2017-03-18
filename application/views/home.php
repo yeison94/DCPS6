4b66a88c1776cdcc29589a28650f8b4b9934bfb7
Jeison <yeison94_andres@hotmail.com>
2017-03-17 20:46:51 -0500

    page home created

---
+++ application/views/home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+    <li>Registrar Granjero</li>
+    <li>Registrar Finca</li>
+    <li>Listar Granjeros</li>
+    <li>Listar Fincas</li>
+    <li>Granjero con mas vacas y gallinas</li>
+    <li>Fincas con mas de 3 gallinas y menos de 5 vacas</li>
+    </ul>
+  </body>
+</html>
