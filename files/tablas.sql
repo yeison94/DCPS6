07fe0742644ab22e73a07102dd513e43f9f4de90
andres1112 <afo_1112@hotmail.com>
2017-03-18 08:52:22 -0500

    Update tablas.sql

--- files/tablas.sql
+++ files/tablas.sql
@@ -19,3 +19,28 @@ CREATE TABLE finca (
 	PRIMARY KEY (id),
 	CONSTRAINT granjero_finca FOREIGN KEY (granjero) REFERENCES granjero (id)
 ) ENGINE = InnoDB;
+
+
+CREATE TABLE curso (
+	id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nombre      VARCHAR(20)  NOT NULL,
+	facultad    VARCHAR(100) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE estudiante (
+	id               INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nombre           VARCHAR(20)  NOT NULL,
+	edad             INT         NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE matricula(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nota_final FLOAT(20) NOT NULL,
+	id_curso INT UNSIGNED NOT NULL,
+	id_estudiante INT UNSIGNED NOT NULL,
+	PRIMARY KEY (id,id_curso,id_estudiante),
+	CONSTRAINT est_curso FOREIGN KEY (id_curso) REFERENCES curso (id),
+	CONSTRAINT est_estud FOREIGN KEY (id_estudiante) REFERENCES estudiante (id)
+) ENGINE = InnoDB;
